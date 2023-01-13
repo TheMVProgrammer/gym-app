bf70c5df3fe94f0c2db556b18fd9a86ce144791a
TheMVProgrammer <franklin0415@hotmail.com>
2023-01-13 12:14:16 -0400

    Finishing exercises component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -4,6 +4,39 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 6;
+
+  const indexOfLastExcercise = currentPage * exercisesPerPage;
+
+  const indexOfFirstExcercise = indexOfLastExcercise - exercisesPerPage;
+
+  const currentExercises = exercises.slice(indexOfFirstExcercise, indexOfLastExcercise);
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: 'smooth' })
+  }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+      
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+
+  }, [bodyPart, setExercises])
+
   return (
     <Box id="exercises"
       sx={{ mt: { lg: '110px' } }}
@@ -19,9 +52,22 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         flexWrap="wrap"
         justifyContent="center"
       >
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+          { exercises.length > exercisesPerPage && (
+            <Pagination 
+              color="standard"
+              shape="rounded"
+              defaultPage={1}
+              count={Math.ceil(exercises.length / exercisesPerPage)}
+              page={currentPage}
+              onChange={paginate}
+              size="large"
+            />
+          ) }
       </Stack>
     </Box>
   )
